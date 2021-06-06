383be669cf2eb5b90747d5105763d6e4259a61cc
Charan Raj K <40881285+Charan-Raj-K@users.noreply.github.com>
2021-06-06 15:16:15 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -8,14 +8,16 @@ resource "aws_instance" "myec2vm" {
     name = "My Linux EC2"
   }
 }
+#The following code will save the auto-generated EC2 private key as a .pem file in our local machine.
 resource "tls_private_key" "ec2_private_key" {
 algorithm = "RSA"
 rsa_bits  = 4096
 provisioner "local-exec" {
-command = "sudo echo '${tls_private_key.ec2_private_key.private_key_pem}' > /home/ec2-user/.ssh/${var.instance_keypair}.pem"
+command = "sudo echo '${tls_private_key.ec2_private_key.private_key_pem}' > /opt/${var.instance_keypair}.pem"
       }
 }
 
+#we will be required to make the key private as by default this file gets saved with public access on the local machine
 resource "null_resource" "key-perm" {
 depends_on = [
 tls_private_key.ec2_private_key,
@@ -25,11 +27,14 @@ command = "sudo chmod 400 /home/ec2-user/.ssh/${var.instance_keypair}.pem"
 
    }
 }
-
-resource "null_resource" "key-auth" {
 provisioner "local-exec" {
 command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/ec2-user/.ssh/${var.instance_keypair}.pem -i '${aws_instance.myec2vm.private_ip},' playbook.yml"
 
    }
-}
+/*resource "null_resource" "key-auth" {
+provisioner "local-exec" {
+command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/ec2-user/.ssh/${var.instance_keypair}.pem -i '${aws_instance.myec2vm.private_ip},' playbook.yml"
+
+   }
+}*/
 
