4e9bff150b6292297af2076929d5d36d6e3942c5
Charan Raj K <40881285+Charan-Raj-K@users.noreply.github.com>
2021-06-06 15:20:32 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,3 +1,6 @@
+
+
+
 resource "aws_instance" "myec2vm" {
   ami                   = data.aws_ami.amzlinux2.id
   instance_type         = var.instance_type
@@ -16,7 +19,12 @@ provisioner "local-exec" {
 command = "sudo echo '${tls_private_key.ec2_private_key.private_key_pem}' > /opt/${var.instance_keypair}.pem"
       }
 }
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
 
+  key_name   = "May"
+  public_key = tls_private_key.ec2_private_key.public_key_openssh
+}
 #we will be required to make the key private as by default this file gets saved with public access on the local machine
 resource "null_resource" "key-perm" {
 depends_on = [
@@ -27,14 +35,12 @@ command = "sudo chmod 400 /home/ec2-user/.ssh/${var.instance_keypair}.pem"
 
    }
 }
+
+resource "null_resource" "Ansible-execution" 
+  {
 provisioner "local-exec" {
 command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/ec2-user/.ssh/${var.instance_keypair}.pem -i '${aws_instance.myec2vm.private_ip},' playbook.yml"
 
    }
-/*resource "null_resource" "key-auth" {
-provisioner "local-exec" {
-command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /home/ec2-user/.ssh/${var.instance_keypair}.pem -i '${aws_instance.myec2vm.private_ip},' playbook.yml"
-
-   }
-}*/
+}
 
